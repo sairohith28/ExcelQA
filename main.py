d05d671539f91154f0de2f2dbf1aea3b8f3f72b8
Sai Rohith <80623182+sairohith28@users.noreply.github.com>
2026-01-19 11:53:57 +0530

    Update main.py Added root path

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import requests
 load_dotenv()
 
 # Initialize FastAPI app
-app = FastAPI(title="Excel QA API", description="Ask questions about your CSV data")
+app = FastAPI(title="Excel QA API", description="Ask questions about your CSV data",root_path="/excelqa")
 
 # Add CORS middleware to allow frontend requests
 app.add_middleware(
